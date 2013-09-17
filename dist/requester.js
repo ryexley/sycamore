6ada0de9487446a5123eda04ecb6d354dde7df18
Bob Yexley <bob@yexley.net>
2013-09-17 12:27:54 -0400

    Grunt build working, little bit of refactoring, and fleshed out bower.json for registration

--- dist/requester.js
+++ dist/requester.js
@@ -1,3 +1,9 @@
+// Sycamore, v0.1.0 | (c) 2013 Bob Yexley
+// Description: A mixin with functionality to wrap jQuery $.ajax calls, and simplify the definition and consumption of $.ajax request options 
+// Generated: 2013-09-17 @ 11:53:09
+// https://github.com/ryexley/sycamore
+// License: http://www.opensource.org/licenses/mit-license
+
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
         define(["jquery", "underscore"], factory);
---
+++ gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON("package.json"),
+
+        meta: {
+            banner: "// <%= pkg.name %>, v<%= pkg.version %> | (c) <%= grunt.template.today('yyyy') %> Bob Yexley\n" +
+                    "// Description: <%= pkg.description %> \n" +
+                    "// Generated: <%= grunt.template.today('yyyy-mm-dd @ h:MM:ss') %>\n" +
+                    "// https://github.com/ryexley/sycamore\n" +
+                    "// License: http://www.opensource.org/licenses/mit-license\n"
+        },
+
+        jshint: {
+            all: {
+                options: {
+                    jshintrc: ".jshintrc",
+                    jshintignore: ".jshintignore"
+                },
+                files: {
+                    source: ["source/*.js"]
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                banner: "<%= meta.banner %>\n"
+            },
+            dist: {
+                src: ["source/requester.js"],
+                dest: "dist/requester.js"
+            }
+        },
+
+        uglify: {
+            dist: {
+                options: {
+                    banner: "<%= meta.banner %>"
+                },
+                files: {
+                    "dist/requester.min.js": ["source/requester.js"]
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ["source/*.js"],
+                tasks: ["default"],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask("default", ["jshint", "build"]);
+    grunt.registerTask("build", ["concat", "uglify"]);
+}
