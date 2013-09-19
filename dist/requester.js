947a7c4eb5df7f78aa0213a88c69f448d633ef23
Bob Yexley <bob@yexley.net>
2013-09-19 14:23:35 -0400

    Release v0.2.0

--- dist/requester.js
+++ dist/requester.js
@@ -1,6 +1,6 @@
-// sycamore, v0.1.10 | (c) 2013 Bob Yexley
+// sycamore, v0.2.0 | (c) 2013 Bob Yexley
 // Description: A mixin with functionality to wrap jQuery $.ajax calls, and simplify the definition and consumption of $.ajax request options 
-// Generated: 2013-09-19 @ 2:18:35
+// Generated: 2013-09-19 @ 2:23:25
 // https://github.com/ryexley/sycamore
 // License: http://www.opensource.org/licenses/mit-license
 
