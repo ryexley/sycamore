8753ddc26e21a9123840268353b305be8f3d3153
Bob Yexley <bob@yexley.net>
2015-06-29 12:50:08 -0400

    Fixed line endings, and added a LICENSE file (MIT)

--- dist/requester.js
+++ dist/requester.js
@@ -1,6 +1,6 @@
-// sycamore, v0.3.8 | (c) 2014 Bob Yexley
+// sycamore, v0.3.8 | (c) 2015 Bob Yexley
 // Description: A mixin with functionality to wrap jQuery $.ajax calls, and simplify the definition and consumption of $.ajax request options 
-// Generated: 2014-11-24 @ 2:57:02
+// Generated: 2015-06-29 @ 12:47:23
 // https://github.com/ryexley/sycamore
 // License: http://www.opensource.org/licenses/mit-license
 
